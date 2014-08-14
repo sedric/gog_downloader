0c0036d1fb84e0dd64eb32411f92433e7e4c0f36
sedric <sedric+github@sedric.fr>
2014-08-14 02:38:53 +0100

    Fix missing shebang

--- download.sh
+++ download.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 cd $(dirname $0)
 cat list.txt | while read url
 do
-    redirect=$(wget -qS --load-cookies=cookies.txt "$url" --max-redirect=0 2>&1 | grep 'Location' | sed -r 's/.*(http[^?]*).*/\1/')
-    filename=$(echo "$redirect" | sed -r 's/(http[^?]*).*/\1/;s/.*\/([^/]*)/\1/')
-    if [ -f "$filename" ]
-    then
-	echo "Skipping $filename"
-    else
-	wget --load-cookies=cookies.txt "$url" -O "$filename"
-    fi
+  redirect=$(wget -qS --load-cookies=cookies.txt "$url" --max-redirect=0 2>&1 | grep 'Location' | sed -r 's/.*(http[^?]*).*/\1/')
+  filename=$(echo "$redirect" | sed -r 's/(http[^?]*).*/\1/;s/.*\/([^/]*)/\1/')
+  if [ -f "$filename" ]
+  then
+    echo "Skipping $filename"
+  else
+    wget --load-cookies=cookies.txt "$url" -O "$filename"
+  fi
 done
